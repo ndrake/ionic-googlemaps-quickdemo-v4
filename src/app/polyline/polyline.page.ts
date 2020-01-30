438af4c8f09a7fd4e7b507d920f282236e881a26
Nate Drake <ndrake@gmail.com>
2020-01-30 14:38:54 -0500

    Demo polyline crash

--- src/app/polyline/polyline.page.ts
+++ src/app/polyline/polyline.page.ts
@@ -8,6 +8,7 @@ import {
   Marker
 } from '@ionic-native/google-maps';
 import { Platform } from '@ionic/angular';
+import { GoogleMapsMapTypeId } from '@ionic-native/google-maps/ngx';
 
 @Component({
   selector: 'app-polyline',
@@ -37,19 +38,77 @@ export class PolylinePage implements OnInit {
       SFO_AIR_PORT
     ];
 
+    let L1 = {lat: 21.327153, lng: -157.928671};
+    let L2 = {lat: 21.327453, lng: -157.925141};
+    let L3 = {lat: 21.325714, lng: -157.925173};
+    let L4 = {lat: 21.325824, lng: -157.927469 };
+
+    AIR_PORTS = [
+      L1, L2, L3, L4
+    ];
+
     this.map = GoogleMaps.create('map_canvas', {
       camera: {
         target: AIR_PORTS
-      }
+      },
+      mapType: GoogleMapsMapTypeId.SATELLITE
+    });
+
+    this.map.addMarkerSync({
+      position: L1,
+      icon: 'green'
+    });
+
+    this.map.addMarkerSync({
+      position: L4,
+      icon: 'green'
     });
 
     let polyline: Polyline = this.map.addPolylineSync({
-      points: AIR_PORTS,
+      points: [L1, L2],
       color: '#AA00FF',
-      width: 10,
-      geodesic: true,
+      width: 4,
+      geodesic: false,
       clickable: true  // clickable = false in default
     });
+
+    polyline = this.map.addPolylineSync({
+      points: [L1, L3],
+      color: '#AAFF00',
+      width: 4,
+      geodesic: false,
+      clickable: true  // clickable = false in default
+    });
+
+    polyline = this.map.addPolylineSync({
+      points: [L1, L4],
+      color: '#00AAFF',
+      width: 4,
+      geodesic: false,
+      clickable: true  // clickable = false in default
+    });
+
+    polyline = this.map.addPolylineSync({
+      points: [L4, L3],
+      color: '#2222FF',
+      width: 4,
+      geodesic: false,
+      clickable: true  // clickable = false in default
+    });
+    polyline = this.map.addPolylineSync({
+      points: [L3, L2],
+      color: '#80FF00',
+      width: 4,
+      geodesic: false,
+      clickable: true  // clickable = false in default
+    });
+    // polyline = this.map.addPolylineSync({
+    //   points: [L3, L1],
+    //   color: '#AAFF00',
+    //   width: 4,
+    //   geodesic: false,
+    //   clickable: true  // clickable = false in default
+    // });
 
     polyline.on(GoogleMapsEvent.POLYLINE_CLICK).subscribe((params: any) => {
       let position: LatLng = <LatLng>params[0];
